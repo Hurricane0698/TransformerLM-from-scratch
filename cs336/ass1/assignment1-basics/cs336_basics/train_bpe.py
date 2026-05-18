6221f5760ac7cec3cd4fc3ac89b82b66808ea134
Omega <tta817944@gmail.com>
2026-05-18 21:46:30 +0800

    Implement byte-level BPE training with parallel pretokenization

--- cs336/ass1/assignment1-basics/cs336_basics/train_bpe.py
+++ cs336/ass1/assignment1-basics/cs336_basics/train_bpe.py
@@ -6,67 +6,68 @@
 
 BPE merge 只能发生在 pre-token 内部。每一轮统计当前 token 序列里的相邻 pair 频率，选择频率最高的 pair；若并列，则选择字典序更大的 pair；然后把所有该 pair 的出现合并成新 token，加入 vocab，并重复直到达到 vocab_size。'''
 import regex as re
-def train_bpe(input_path:str, 
-              vocab_size:int, 
-              special_tokens:list) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-    vocab = {i: bytes([i]) for i in range(256)}
-    with open(input_path, "r", encoding="utf-8") as f:
-        train_data = f.read()
-    #按空和非空list逻辑处理
-    if special_tokens:
-        #先处理<|endoftext|>里的特殊元素防止分错，再按re.split|逻辑来分
-        train_data_list = re.split(("|".join([re.escape(token) for token in special_tokens])), 
-                            train_data)
+from pretokenization_example import find_chunk_boundaries
+from collections import Counter
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+import pickle
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+def count_chunk(job):
+    input_path, start, end, special_tokens= job
+    with open(input_path, "rb") as f:
+        f.seek(start)
+        chunk = f.read(end - start).decode("utf-8", errors="ignore")
+    if not special_tokens:
+            data_list = [chunk] #[str, str]
     else:
-        train_data_list = [train_data]
-    #pre-tokenization
-    count = {} 
-    ''' count:
-            token_tuple -> frequency
-        pair_counts:
-            pair -> total_frequency
-        pair_to_words:
-            pair -> set of token_tuple that currently contain this pair'''
-    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-    #count初始化
-    for data in train_data_list:
-        #encode，计数
-        for match in re.finditer(PAT, data):
-            pre_token = match.group().encode("utf-8") #eg:b'a'
-            pre_token_list = []
-            for i in pre_token:
-                pre_token_list.append(bytes([i]))
-            pre_token_tuple = tuple(pre_token_list)
-            if pre_token_tuple in count:
-                count[pre_token_tuple] += 1
-            else:
-                count[pre_token_tuple] = 1
+        data_list = re.split("|".join([re.escape(special_token) for special_token in special_tokens]), chunk)
+    #counts{ (b'',...):x }
+    chunk_counts = Counter()
+    #初始化pre-tokenization, count同时计数
+    for data in data_list:
+        for match in re.finditer(PAT, data): 
+            #pre_token_list = re.split(PAT, data)#["a", "Hello", "you-are", ","]
+            encoded_pre_token = match.group().encode("utf-8")#b'ab/x0e'
+            pre_token_tuple = tuple([bytes([i]) for i in encoded_pre_token])
+            chunk_counts[pre_token_tuple] += 1
+    return chunk_counts
+
+def train_bpe(input_path:str, vocab_size:int, special_tokens:list) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    vocab = {i : bytes([i]) for i in range(256)}
+    #打开路径获得数据集，确定边界，把jobs下发给子进程
+    with open(input_path, "rb") as f:
+        num_workers = 8
+        desired_num_chunks = num_workers * 4
+        boundaries = find_chunk_boundaries(f, desired_num_chunks, b"<|endoftext|>")
+    total_counts = Counter()
+    jobs = [(input_path, start, end, special_tokens)
+            for start, end in zip(boundaries[:-1], boundaries[1:])]
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        partial_counts = executor.map(count_chunk, jobs)
+    for partial_count in partial_counts:
+        total_counts.update(partial_count)
+    counts = total_counts
 
     merge_list = []
-    pair_counts = {} #{(b'a', b'b'):x}
-    pair_to_words = {}#{(b'a', b'b'):set((b'a', b'b', b'c'),(b'g', b'a', b'b'),....)}
-
-    #pair_counts和pair_to_words初始化
-    for per_token_tuple in count: #(b'a', b'c', b'd')
-        if len(per_token_tuple) >= 2:#先判断元组不是单元素
-            for i in range(0, len(per_token_tuple)-1):
-                sub_tuple = (per_token_tuple[i], per_token_tuple[i+1])
-                if sub_tuple not in pair_to_words:
-                    pair_to_words[sub_tuple] = set()
-                pair_to_words[sub_tuple].add(per_token_tuple)
-                if sub_tuple in pair_counts:
-                    pair_counts[sub_tuple] += count[per_token_tuple]
-                else:
-                    pair_counts[sub_tuple] = count[per_token_tuple] 
-        else:
-            continue
-
-    #merge best pair, 更新count值、pair_counts、pair_to_words三个全局状态
-    while len(vocab) < vocab_size-len(special_tokens):
-        #先判断有pair可选
+    #初始化pair_counts和pair_to_words
+    pair_counts = Counter()#{(b'a', b'c'):14, ...}
+    pair_to_words = {}#{(b'c', b'd'):set((b'a', b'b', b'c', b'd')), ...}
+    for word_tuple in counts:
+        if len(word_tuple) >= 2:
+            for i in range(len(word_tuple)-1):
+                pair = (word_tuple[i], word_tuple[i+1])
+                pair_counts[pair] += counts[word_tuple]
+                
+                if pair not in pair_to_words:
+                    pair_to_words[pair] = set()
+                pair_to_words[pair].add(word_tuple)
+    
+    #merge循环开始：
+    while len(vocab) < vocab_size - len(special_tokens):
+        #先确认pair_counts里面仍然有值
         if not pair_counts:
             break
-        #贪心更新
+        #贪心取值找best_pair
         best_pair = None
         best_freq = None
         for candidate_pair, candidate_freq in pair_counts.items():
@@ -78,72 +79,62 @@ def train_bpe(input_path:str,
                 best_freq = candidate_freq
             elif candidate_freq == best_freq and candidate_pair > best_pair:
                 best_pair = candidate_pair
-
-        #词汇表增加merge后的bytes
         expected = best_pair[0] + best_pair[1] #b'ab'
-        vocab[len(vocab)] = expected
+        #维护merge_list
         merge_list.append(best_pair)
-        
-        #对于影响的word，定向merge best_pair
-        affected_words = pair_to_words[best_pair].copy()#set((b'a', b'b', b'c'),(b'g', b'a', b'b'),....),用copy是因为=是指针，copy才是副本，迭代不会出问题
-        for affected_word in affected_words:
-            i = 0 #维护扫描更新merge_word的指针
+        #加词表
+        vocab[len(vocab)] = expected
+        #维护三个不变量
+        old_words = pair_to_words[best_pair].copy()#set((a,b),(a,b,c), (a,b,a,b)),必须copy，等于是指针，如果直接在下面循环使用会边改边迭代
+        for old_word in old_words:
+            #先去除pair_counts里旧词的贡献
+            for i in range(len(old_word)-1):
+                old_pair = (old_word[i], old_word[i+1])
+                freq = counts[old_word]
+                pair_counts[old_pair] -= freq
+                if pair_counts[old_pair] == 0:
+                    del pair_counts[old_pair]
+            #再维护pair_to_words,按mermbership维护
+            unique_old_pairs = set()
+            for i in range(len(old_word)-1):
+                old_pair = (old_word[i], old_word[i+1])
+                unique_old_pairs.add(old_pair)
+            for unique_old_pair in unique_old_pairs:
+                pair_to_words[unique_old_pair].remove(old_word)
+                if not pair_to_words[unique_old_pair]:
+                    del pair_to_words[unique_old_pair]
 
-            #pair_to_words 只维护 membership，所以同一个 old word 对同一个 pair 只 remove 一次
-            old_pairs = set()
-            for p in range(len(affected_word)-1):
-                key = (affected_word[p], affected_word[p+1])
-                old_pairs.add(key)
-            for k in old_pairs:
-                pair_to_words[k].remove(affected_word)
-                if not pair_to_words[k]:
-                    del pair_to_words[k]
-            
-            #先读贡献，再撤销pair在pair_counts里的贡献
-            frequency = count[affected_word]
-            new_per_token_list = []
-            for c in range(len(affected_word)-1):
-                pair = (affected_word[c], affected_word[c+1])
-                pair_counts[pair] -= frequency
-                if pair_counts[pair] == 0:#如果值已经清零就删掉
-                    del pair_counts[pair]
-            
-            #把含best_pair的词更新        
-            while i <= len(affected_word)-1:
-                if i <= len(affected_word) - 2:
-                    if best_pair == (affected_word[i], affected_word[i+1]):
-                        new_per_token_list.append(expected)
+            #先维护counts
+            i = 0
+            new_word_list = []
+            while i <= len(old_word) - 1:
+                if i <= len(old_word) - 2:
+                    if best_pair == (old_word[i], old_word[i+1]):
+                        new_word_list.append(expected)
                         i += 2
                     else:
-                        new_per_token_list.append(affected_word[i])
+                        new_word_list.append(old_word[i])
                         i += 1
-                elif i == len(affected_word)-1:
-                    new_per_token_list.append(affected_word[i])
-                    i += 1
-            new_per_token_tuple = tuple(new_per_token_list)
+                else:
+                        new_word_list.append(old_word[i])
+                        i += 1
+            new_word = tuple(new_word_list)
+            freq = counts.pop(old_word)
+            counts[new_word] += freq
             
-            #使用新的new_per_token_tuple计算新的贡献并更新pair_counts、pair_to_words
-            for j in range(len(new_per_token_tuple)-1):
-                    new_pair = (new_per_token_tuple[j], new_per_token_tuple[j+1])#(b'ab', b'c')
-                    #更新pair_to_word映射
-                    if new_pair not in pair_to_words:
-                        pair_to_words[new_pair] = set()
-                    pair_to_words[new_pair].add(new_per_token_tuple)
-                    #更新pair_counts映射
-                    if new_pair in pair_counts:
-                        pair_counts[new_pair] += frequency
-                    else:
-                        pair_counts[new_pair] = frequency
-            freq = count.pop(affected_word)
-            
-            #更新count
-            if new_per_token_tuple in count:
-                count[new_per_token_tuple] += freq
-            else:
-                count[new_per_token_tuple] = freq
-    #词表最后加上特殊符号
+            #再维护pair_counts更新
+            for i in range(len(new_word)-1):
+                new_pair = (new_word[i], new_word[i+1])
+                pair_counts[new_pair] += freq
+            #再维护pair_to_words更新
+            for i in range(len(new_word)-1):
+                new_pair = (new_word[i], new_word[i+1])
+                if new_pair not in pair_to_words:
+                    pair_to_words[new_pair] = set()
+                pair_to_words[new_pair].add(new_word)
+    #最后加上special_token
     for special_token in special_tokens:
-        vocab[len(vocab)] = special_token.encode("utf-8")#str->bytes
+        vocab[len(vocab)] = special_token.encode("utf-8")
     return vocab, merge_list
 
 '''BPE 训练维护三个长期状态：
@@ -165,3 +156,17 @@ pair_to_words: 当前 pair -> 包含它的 token tuple 集合
    - 在 count 中删除 old word，把频率加到 new word。
 5. 最后追加 special tokens。
 '''
+if __name__ == "__main__":
+    BASE_DIR = Path(__file__).resolve().parent
+    input_path = BASE_DIR.parent / "data" / "TinyStoriesV2-GPT4-train.txt"
+    vocab, merge_list = train_bpe(str(input_path), 10000, ["<|endoftext|>"])
+
+    with open("tinystories_vocab.pkl", "wb") as f:
+        pickle.dump(vocab, f)
+
+    with open("tinystories_merges.pkl", "wb") as f:
+        pickle.dump(merge_list, f)
+    longest_token = max(vocab.values(), key=len)
+    print(len(longest_token))
+    print(longest_token)
+    print(longest_token.decode("utf-8", errors="replace"))
